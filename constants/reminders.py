79955169cf9137256757a6f2774d5825834a8f2a
Ian Man <ian.man@hva.nl>
2022-10-11 22:42:48 +0200

    changed reminders text

--- constants/reminders.py
+++ constants/reminders.py
@@ -1,7 +1,8 @@
 REMINDERS = {
     "first_day": {
         "text": lambda user_id, mentions: f"Het is vandaag renewal dag, <@{user_id}>! Je hebt nog 5 dagen om te betalen"
-                                          f", anders moeten wij helaas je licentie verwijderen! {mentions}",
+                                          f", anders moeten wij helaas je licentie verwijderen! Laat wel even weten of je betaald hebt! Alvast bedankt!"
+                                          f"{mentions}",
         "dm_text": lambda user_id, channel_id: f"Het is vandaag renewal dag, <@{user_id}>! Check <#{channel_id}> om te "
                                                f"betalen.",
         "day_difference": 0
@@ -9,7 +10,8 @@ REMINDERS = {
     "second_day": {
         "text": lambda user_id, mentions: f"Renewal dag is geweest <@{user_id}>! Betaal zo snel mogelijk het betaalverz"
                                           f"oek zodat we lekker snel door kunnen gaan met koken. Je hebt nog 3 dagen om"
-                                          f" te betalen. Als het langer duurt moeten wij helaas je licentie verwijderen.",
+                                          f" te betalen. Als het langer duurt moeten wij helaas je licentie verwijderen"
+                                          f". Laat wel even weten of je betaald hebt! Alvast bedankt!",
         "dm_text": lambda user_id, channel_id: f"Renewal dag is geweest <@{user_id}>! Je hebt nog 3 dagen om te betalen"
                                                f". Check <#{channel_id}> om te betalen.",
         "day_difference": 1
@@ -17,7 +19,8 @@ REMINDERS = {
     "fourth_day": {
         "text": lambda user_id, mentions: f"Renewal dag is geweest <@{user_id}>! Betaal zo snel mogelijk het betaalverz"
                                           f"oek zodat we lekker snel door kunnen gaan met koken. Je hebt nog 1 dag om t"
-                                          f"e betalen. Als het langer duurt moeten wij helaas je licentie verwijderen.",
+                                          f"e betalen. Als het langer duurt moeten wij helaas je licentie verwijderen. "
+                                          f"Laat wel even weten of je betaald hebt! Alvast bedankt!",
         "dm_text": lambda user_id, channel_id: f"Renewal dag is geweest <@{user_id}>! Je hebt nog 1 dag om te betalen"
                                                f". Check <#{channel_id}> om te betalen.",
         "day_difference": 3
@@ -25,7 +28,7 @@ REMINDERS = {
     "sixth_day": {
         "text": lambda user_id, mentions: f"Helaas heb je niet betaald voor de renewal <@{user_id}>. Je licentie wordt "
                                           f"nu verwijderd. Hopelijk zien we je snel terug! Dit kanaal wordt na een dag"
-                                          f" verwijderd.",
+                                          f" verwijderd. Laat wel even weten of je betaald hebt! Alvast bedankt!",
         "dm_text": lambda user_id, channel_id: f"Helaas heb je niet betaald voor de renewal <@{user_id}>. Je licentie w"
                                                f"ordt nu verwijderd. Hopelijk zien we je snel terug!",
         "day_difference": 5
